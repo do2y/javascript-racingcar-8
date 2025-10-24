3bdd06439bc819e161ec48b16c7f67254b5ada54
Sohee Lee <essen030216@naver.com>
2025-10-24 20:00:57 +0900

    feat(App.js): 자동차 이름을 쉼표 기준으로 파싱하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async run() {
     const carNamesInput = await this.getCarNames();
+    const carNames = this.parseCarNames(carNamesInput);
+    console.log(carNames);
   }
 
   async getCarNames() {
@@ -10,6 +12,10 @@ class App {
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
     return input.trim();
+  }
+
+  parseCarNames(input) {
+    return input.split(',').map((name) => name.trim());
   }
 }
 
