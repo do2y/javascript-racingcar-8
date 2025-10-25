f0e294056436a428adb0e514a76d22da58911873
Sohee Lee <essen030216@naver.com>
2025-10-25 16:19:16 +0900

    chore(error.js): 에러 메시지 상수 정의

---
+++ src/error.js
@@ -0,0 +1,11 @@
+export const ERROR_MESSAGES = {
+  INVALID_DELIMITER: '[ERROR] 구분자는 쉼표(,)만 사용할 수 있습니다.',
+  INVALID_COMMA_POSITION: '[ERROR] 쉼표(,)는 이름의 앞뒤에 올 수 없습니다.',
+
+  EMPTY_CAR_NAME: '[ERROR] 빈 이름은 입력할 수 없습니다.',
+  CAR_NAME_TOO_LONG: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
+  NO_CAR_NAME: '[ERROR] 자동차 이름이 입력되지 않았습니다.',
+
+  EMPTY_ROUND_COUNT: '[ERROR] 시도 횟수가 입력되지 않았습니다.',
+  INVALID_ROUND_COUNT: '[ERROR] 시도 횟수는 0 이상의 정수만 가능합니다.',
+};
