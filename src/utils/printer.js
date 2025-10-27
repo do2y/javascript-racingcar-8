1ba720792643c993ab6f5df62ec51b06e7a91b51
Sohee Lee <essen030216@naver.com>
2025-10-27 14:29:52 +0900

    refactor(printer.js): 출력 로직을 Printer 객체로 리팩터링

--- src/utils/printer.js
+++ src/utils/printer.js
@@ -1,10 +1,18 @@
 import { Console } from '@woowacourse/mission-utils';
 
-export const printWinners = (carNames, carPositions) => {
-  const maxDistance = Math.max(...carPositions.map((pos) => pos.length));
-  const winners = carNames.filter(
-    (_, index) => carPositions[index].length === maxDistance,
-  );
+export const Printer = {
+  resultTitle() {
+    Console.print('\n실행 결과');
+  },
 
-  Console.print(`최종 우승자 : ${winners.join(', ')}`);
+  roundResult(carNames, positions) {
+    carNames.forEach((name, idx) =>
+      Console.print(`${name} : ${positions[idx]}`),
+    );
+    Console.print('');
+  },
+
+  winners(winners) {
+    Console.print(`최종 우승자 : ${winners.join(', ')}`);
+  },
 };
